4328bff18a2da5ebe5701cc9c5aa896514bd49e9
Alan-Ou <156554822@qq.com>
2023-12-08 17:41:42 +0800

    wrap project into jar

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ADB
-A distributed database
+## Replicated Concurrency Control and Recovery
+### Programming Language 
+`Java`: `jdk 1.8`
+### Running
+
+- We have provided the jar file, so you could run by typing: `java -jar <input_file>`
+- Otherwise, use normal approach: clone it, open it by IDE and try `build` and `run`.
+
+### Project Introduction
+For more information, please see: https://cs.nyu.edu/courses/fall23/CSCI-GA.2434-001/handDB2.pdf
+
+
--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -5,54 +5,33 @@ import com.example.lock.LockError;
 import com.example.transaction.TransactionError;
 import com.example.transaction.TransactionManager;
 import com.example.utils.ParserError;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            TransactionManager manager = new TransactionManager();
-            System.out.println("Please input file path (or 'exit' to quit):");
-            String inputFilePath = scanner.nextLine();
-
-            if (inputFilePath.equalsIgnoreCase("exit")) {
-                break;
-            }
-
-            try {
-                System.out.println("Getting inputs from " + inputFilePath);
-                BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    manager.process(line);
-                }
-                reader.close();
-
-                System.out.print("Process another file [y/n]? ");
-                String isContinue = scanner.nextLine();
-                if (isContinue.equalsIgnoreCase("n")) {
-                    break;
-                }
-            } catch (IOException e) {
-                System.out.println("Error, cannot open " + inputFilePath);
-            } catch (DataError e) {
-                throw new RuntimeException(e);
-            } catch (ParserError e) {
-                throw new RuntimeException(e);
-            } catch (TransactionError e) {
-                throw new RuntimeException(e);
-            } catch (LockError e) {
-                throw new RuntimeException(e);
-            }
+        if (args.length != 1) {
+            System.err.println("Using jar file: java -jar ADB.jar <input_file>");
+            System.err.println("Using IDE: specify argument <input_file>");
+            System.exit(1);
         }
 
-        System.out.println("Bye");
-        scanner.close();
+        String inputFilePath = args[0];
+        TransactionManager manager = new TransactionManager();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                manager.process(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error, cannot open " + inputFilePath);
+            e.printStackTrace();
+            System.exit(1);
+        } catch (DataError | ParserError | TransactionError | LockError e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Processing completed.");
     }
 }
